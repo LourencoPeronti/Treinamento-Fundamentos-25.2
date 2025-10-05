aeeeaa7a163eadb64dbaed8220daa668f58cd954
LourencoPeronti <lourencoperonti@gmail.com>
2025-10-04 23:33:26 -0300

    commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,42 @@
+
+
 const produtos = [
     {
+        id: "0",
         nome: "Poção Anti-DP",
         preco: "R$2000,00",
         descricao: "Tome e fique imune de DPs para SEMPRE",
         img: "assets/imgs/pocao-removebg-preview.png",
+        isOnCarrinho: false
     },
     {
+        id: "1",
         nome: "Óculos Vetorial",
         preco: "R$1550,39",
-        descricao: "Dificuldade em vizualizar os vetores em AlgLin? Este óculos permite vizualizá-los no espaçco bem na sua frente",
-        img: "assets/imgs/pocao-removebg-preview.png"
+        descricao: "Dificuldade em vizualizar os vetores em AlgLin? Este óculos permite vizualizá-los no espaço bem na sua frente",
+        img: "assets/imgs/oculos_vetorial.png",
+        isOnCarrinho: false
     },
     {
-        nome: "Óculos Vetorial",
+        id: "2",
+        nome: "Relógio Dobrado",
         preco: "R$1550,39",
-        descricao: "Dificuldade em vizualizar os vetores em AlgLin? Este óculos permite vizualizá-los no espaçco bem na sua frente",
-        img: "assets/imgs/pocao-removebg-preview.png"
+        descricao: "Não consegue fazer tudo em 24h? Não se preocupe, este relógio deixa seu dia com 48h (Talvez ainda não seja suficiente para o politécnico)",
+        img: "assets/imgs/pocao-removebg-preview.png",
+        isOnCarrinho: false
     },
     
     
 ]
 
+var produtos_carrinho = 0
+var img_botao = "assets/imgs/adc_carrinho.png"
+
+
 const container = document.getElementsByClassName("containerCorpo")[0];
 const card = document.getElementsByClassName("produto")[0];
 container.innerHTML = "";
+
 
 produtos.forEach(produto => {
     const item = card.cloneNode(true);
@@ -32,6 +45,30 @@ produtos.forEach(produto => {
     item.querySelector(".preco").textContent = produto.preco;
     item.querySelector(".desc").textContent = produto.descricao;
     item.querySelector(".imagem").src = produto.img;
+    item.querySelector(".img_botao").src = "assets/imgs/adc_carrinho.png"
+    item.querySelector(".botao").id = produto.id
 
     container.appendChild(item);
+});
+
+const botoes = document.querySelectorAll(".botao")
+botoes.forEach(botao => {
+  botao.addEventListener('click', () => {
+    id = Number(botao.id)
+    if (produtos[id].isOnCarrinho == false) {
+        produtos_carrinho++
+        document.getElementById("txt_carrinho").innerText = String(produtos_carrinho)
+        produtos[id].isOnCarrinho = true
+        botao.classList.add("apertado")
+        botao.querySelector(".img_botao").src = "assets/imgs/remove_carrinho.png"
+    }
+    else if (produtos[id].isOnCarrinho == true) {
+        produtos_carrinho--
+        document.getElementById("txt_carrinho").innerText = String(produtos_carrinho)
+        produtos[id].isOnCarrinho = false
+        botao.classList.remove("apertado")
+        botao.querySelector(".img_botao").src = "assets/imgs/adc_carrinho.png"
+    }
+    
+  });
 });
